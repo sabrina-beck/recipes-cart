91d833100b9df5bc750adb1321695d4420531135
Sabrina Beck Angelini <sabrinabeck.96@gmail.com>
2025-07-28 18:10:36 -0300

    catch unexpected exceptions on http server

--- input/http-server/src/main/kotlin/com/recipescart/httpserver/errors/ErrorResponse.kt
+++ input/http-server/src/main/kotlin/com/recipescart/httpserver/errors/ErrorResponse.kt
@@ -6,6 +6,7 @@ enum class Error(
     RECIPE_NOT_FOUND("Recipe not found"),
     CART_NOT_FOUND("Cart not found"),
     ILLEGAL_ARGUMENT,
+    INTERNAL_SERVER_ERROR("An unexpected error occurred. Please try again later."),
 }
 
 data class ErrorResponse(
--- input/http-server/src/main/kotlin/com/recipescart/httpserver/errors/GlobalExceptionHandler.kt
+++ input/http-server/src/main/kotlin/com/recipescart/httpserver/errors/GlobalExceptionHandler.kt
@@ -1,5 +1,6 @@
 package com.recipescart.httpserver.errors
 
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -7,10 +8,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
 class GlobalExceptionHandler {
+
+    private val logger = LoggerFactory.getLogger(GlobalExceptionHandler::class.java)
+
     @ExceptionHandler(IllegalArgumentException::class)
     fun handleIllegalArgument(ex: IllegalArgumentException): ResponseEntity<ErrorResponse> =
         ResponseEntity(
             ErrorResponse(error = Error.ILLEGAL_ARGUMENT, message = ex.message ?: "Unknown illegal argument"),
             HttpStatus.BAD_REQUEST,
         )
+
+    @ExceptionHandler(Exception::class)
+    fun handleUnexpectedException(ex: Exception): ResponseEntity<ErrorResponse> {
+        logger.error("Unexpected error occurred", ex)
+
+        return ResponseEntity(
+            ErrorResponse(
+                error = Error.INTERNAL_SERVER_ERROR,
+                message = Error.INTERNAL_SERVER_ERROR.message!!
+            ),
+            HttpStatus.INTERNAL_SERVER_ERROR,
+        )
+    }
 }
